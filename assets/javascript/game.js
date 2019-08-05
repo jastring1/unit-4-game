3588c0c199ddc7ec769ee0e396c442feffe5bca7
jastring <tripp.stringfield@gmail.com>
2019-08-05 18:24:45 -0400

    added more javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,84 @@ var gameTotal = 0;
 var wins;
 var losses;
 
-var directionsText = document.getElementById("title-text");
+var titleText = document.getElementById("title-text");
 var directionsText = document.getElementById("directions-text");
-var directionsText = document.getElementById("target-text");
-var directionsText = document.getElementById("winloss-text");
-var directionsText = document.getElementById("running-text");
+var targetText = document.getElementById("target-text");
+var winlossText = document.getElementById("winloss-text");
+var runningText = document.getElementById("running-text");
+
+function resetGame() {
+    targetScore = Math.floor(Math.random() * 120 + 19);
+    gem1 = Math.floor(Math.random() * 12 + 1);
+    gem2 = Math.floor(Math.random() * 12 + 1);
+    gem3 = Math.floor(Math.random() * 12 + 1);
+    gem4 = Math.floor(Math.random() * 12 + 1);
+    gameTotal = 0;
+}
+
+function updateScoreboard() {
+    targetText.textContent = targetScore;
+    winlossText.textContent = "Wins: " + wins +"<br>Losses: " + losses;
+    runningText.textContent = gameTotal;
+
+}
+$(document).ready(function () {
+
+    $("#gem1").on("click", function () {
+        gameTotal += gem1;
+        if (gameTotal === targetScore) {
+            wins++;
+            resetGame();
+        }
+        else if (gameTotal > targetScore) {
+            losses++;
+            resetGame();
+        }
+        else {
+            updateScoreboard();
+        }
+    });
+    $("#gem2").on("click", function () {
+        gameTotal += gem2;
+        if (gameTotal === targetScore) {
+            wins++;
+            resetGame();
+        }
+        else if (gameTotal > targetScore) {
+            losses++;
+            resetGame();
+        }
+        else {
+            updateScoreboard();
+        }
+    });
+    $("#gem3").on("click", function () {
+        gameTotal += gem3;
+        if (gameTotal === targetScore) {
+            wins++;
+            resetGame();
+        }
+        else if (gameTotal > targetScore) {
+            losses++;
+            resetGame();
+        }
+        else {
+            updateScoreboard();
+        }
+    });
+    $("#gem4").on("click", function () {
+        gameTotal += gem4;
+        if (gameTotal === targetScore) {
+            wins++;
+            resetGame();
+        }
+        else if (gameTotal > targetScore) {
+            losses++;
+            resetGame();
+        }
+        else {
+            updateScoreboard();
+        }
+    });
+});
+
