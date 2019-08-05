f723a2f48a6d043cd47bb08bf3a9f49f1d8c13b3
jastring <tripp.stringfield@gmail.com>
2019-08-05 17:40:33 -0400

    added some html and javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var targetScore = 0;
+var gem1 = 0;
+var gem2 = 0;
+var gem3 = 0;
+var gem4 = 0;
+var gameTotal = 0;
+var wins;
+var losses;
+
+var directionsText = document.getElementById("title-text");
+var directionsText = document.getElementById("directions-text");
+var directionsText = document.getElementById("target-text");
+var directionsText = document.getElementById("winloss-text");
+var directionsText = document.getElementById("running-text");
