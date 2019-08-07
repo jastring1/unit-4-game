49390c6976f805e9bc9394f418bf7729757c3b48
jastring <tripp.stringfield@gmail.com>
2019-08-07 18:03:06 -0400

    more efficiency added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,8 @@
 var targetScore = 0;
-var gem1 = 0;
-var gem2 = 0;
-var gem3 = 0;
-var gem4 = 0;
+var gemValues = [0, 0, 0, 0];
 var gameTotal = 0;
-var wins =0;
-var losses =0;
+var wins = 0;
+var losses = 0;
 
 var targetText = document.getElementById("target-text");
 var winlossText = document.getElementById("winloss-text");
@@ -13,61 +10,47 @@ var runningText = document.getElementById("running-text");
 
 function resetGame() {
     targetScore = Math.floor(Math.random() * 102 + 19);
-    gem1 = Math.floor(Math.random() * 12 + 1);
-    gem2 = Math.floor(Math.random() * 12 + 1);
-    gem3 = Math.floor(Math.random() * 12 + 1);
-    gem4 = Math.floor(Math.random() * 12 + 1);
+    for (var i = 0; i < 4; i++) { gemValues[i] = Math.floor(Math.random() * 12 + 1); }
     gameTotal = 0;
 }
-
 function updateScoreboard() {
     targetText.textContent = targetScore;
-    winlossText.textContent = "Wins: " + wins +"   Losses: " + losses;
+    winlossText.textContent = "Wins: " + wins + "   Losses: " + losses;
     runningText.textContent = gameTotal;
-
 }
-
-function checkResults(){
+function checkResults() {
     if (gameTotal === targetScore) {
         wins++;
-        resetGame(); 
+        resetGame();
     }
     else if (gameTotal > targetScore) {
         losses++;
         resetGame();
     }
-    else {
-        
-    }
     updateScoreboard();
-
 }
 targetScore = Math.floor(Math.random() * 101 + 19);
 targetText.textContent = targetScore;
-winlossText.textContent = "Wins: " + wins +"   Losses: " + losses;
-gem1 = Math.floor(Math.random() * 12 + 1);
-gem2 = Math.floor(Math.random() * 12 + 1);
-gem3 = Math.floor(Math.random() * 12 + 1);
-gem4 = Math.floor(Math.random() * 12 + 1);
+winlossText.textContent = "Wins: " + wins + "   Losses: " + losses;
+for (var i = 0; i < 4; i++) { gemValues[i] = Math.floor(Math.random() * 12 + 1); }
 gameTotal = 0;
 
 $(document).ready(function () {
 
     $("#gem1").on("click", function () {
-        gameTotal += gem1;
+        gameTotal += gemValues[0];
         checkResults();
-        
     });
     $("#gem2").on("click", function () {
-        gameTotal += gem2;
+        gameTotal += gemValues[1];
         checkResults();
     });
     $("#gem3").on("click", function () {
-        gameTotal += gem3;
+        gameTotal += gemValues[2];
         checkResults();
     });
     $("#gem4").on("click", function () {
-        gameTotal += gem4;
+        gameTotal += gemValues[3];
         checkResults();
     });
 });
